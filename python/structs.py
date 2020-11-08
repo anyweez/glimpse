b66ccc2457ad3ddb375d7ea840d02b2fef5971a5
anyweez <luke@lukesegars.com>
2020-11-08 13:45:46 -0500

    Basic plate tectonics + beginning to abstract world graph.
    Plates allow us to generate distinct terrain on each plate. I'm generating
    a world graph for each plate but need to adapt world generation functions
    to pull from world graphs instead of the whole world.

--- python/structs.py
+++ python/structs.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-import enum, random, math, colour, collections
+import enum, random, math, colour, collections, pprint
 
 from scipy.spatial import voronoi_plot_2d
 
@@ -22,10 +22,75 @@ class Cell(object):
         self.type = Cell.Type.NONE
         self.elevation = 0
 
-    def add_edge(self, cell):
-        # Can't add existing neighbors, and can't add self
-        if cell not in self.neighbor_to and cell.region_idx is not self.region_idx:
-            self.neighbor_to.append(cell)
+        # What tectonic plate is this cell a part of?
+        self.plate_id = None
+
+class WorldRegion(object):
+    def __init__(self, cells, vor):
+        self.cellmap = {}
+        for cell in cells:
+            self.cellmap[cell.region_idx] = cell
+        
+        self.cells = cells
+        self.vor = vor
+
+        # 'Neighbor map': Map the idx of the cell in self.cells to the idx of all neighbors
+        self.nmap = {}
+
+        self._build_graph()
+
+    def _add_edge(self, src_idx, dest_idx):
+        if src_idx not in self.nmap:
+            self.nmap[src_idx] = set()
+
+        self.nmap[src_idx].add(dest_idx)
+
+    def _build_graph(self):
+        for source in self.cells:
+            for dest in self.cells:
+                if source.region_idx != dest.region_idx:
+                    source_region = set( self.vor.regions[source.region_idx] )
+                    dest_region = set( self.vor.regions[dest.region_idx] )
+
+                    if -1 in source_region:
+                        source_region.remove(-1)
+                    if -1 in dest_region:
+                        dest_region.remove(-1)
+
+                    if not source_region.isdisjoint(dest_region):
+                        self._add_edge(source.region_idx, dest.region_idx)
+                        self._add_edge(dest.region_idx, source.region_idx)
+
+    def neighbors(self, region_idx, dist=1):
+        # If we're looking for immediate neighbors, jump straight to the nmap where this is explicitly
+        # known. This also allows us to use neighbors(dist=1) for calculations when dist != 1
+        if dist == 1:
+            return list( map(lambda idx: self.cellmap[idx], self.nmap[region_idx]) )
+
+        by_distance = []
+        added = set([self.cellmap[region_idx],])
+
+        for curr_dist in range(dist):
+            if curr_dist == 0:
+                by_distance.append( self.neighbors(region_idx, dist=1) )
+
+                for c in by_distance[curr_dist]:
+                    added.add(c)
+
+            else:
+                next_round = []
+                for parent in by_distance[curr_dist - 1]:
+                    for cell in self.neighbors(parent.region_idx, dist=1):
+                        next_round.append(cell)
+
+                next_round = list( set(next_round) )
+                # De-duplicate and flatten a list of the neighbors of all cells
+                by_distance.append( [c for c in next_round if c not in added] )
+
+                for c in by_distance[curr_dist]:
+                    added.add(c)
+
+        return by_distance[dist - 1]
 
 '''
 Worlds contain a series of Cells, and store the Voronoi diagram used to define the region
@@ -39,9 +104,68 @@ class World(object):
 
         self._form_cells()
         self._label_boundary()
-        self._add_ocean()
-        self._terrainify()
-        self._add_elevation()
+
+        # Establish tectonic plates. Once this is done we'll create a separate region for each.
+        wr = WorldRegion(self.cells, self.vor)
+        self._form_plates(wr)
+
+        for plate_id in set([c.plate_id for c in self.cells]):
+            cells = [c for c in self.cells if c.plate_id == plate_id]
+
+            region = WorldRegion(cells, self.vor)
+
+            self._add_ocean(region)
+            self._terrainify(region)
+        # Run world generation functions on each plate independently.
+
+        # self._add_ocean()
+        # self._terrainify()
+        # self._add_elevation()
+
+
+    def _form_plates(self, region):
+        plate_centers = random.choices(region.cells, k=3)
+
+        for idx, center in enumerate(plate_centers):
+            center.plate_id = idx
+
+        # Function to check whether a cell still needs to have a plate assigned. Boundary cells
+        # are excluded.
+        still_available = lambda cell: cell.plate_id is None
+
+        remaining = sum( [1 for cell in region.cells if still_available(cell)] )
+
+        # Add all cells to a plate
+        while remaining > 0:
+            # For each plate, expand out from the center
+            for center in plate_centers:
+                dist = 1
+                avail = [c for c in region.neighbors(center.region_idx, dist) if still_available(c)]
+
+                while len(avail) == 0:
+                    dist += 1
+                    avail = [c for c in region.neighbors(center.region_idx, dist) if still_available(c)]
+
+                mark = random.choice(avail)
+                mark.plate_id = center.plate_id
+
+                # Count remaining cells to be marked; break the loop if we're done.
+                remaining = sum( [1 for cell in region.cells if still_available(cell)] )
+
+                if remaining == 0:
+                    break
+
+            print(remaining)
+            # There's a chance to add a new plate each iteration. New plates
+            # can only exist in unmarked cells.
+            # if random.random() < 0.05:
+            #     avail = [c for c in self.cells if c.plate_id is None]
+
+            #     if len(avail) > 0:
+            #         cell = random.choice(avail)
+                    
+            #         cell.plate_id = len(plate_centers)
+            #         plate_centers.append(cell)
 
     '''
     Generate a series of world cells based on the specified Voronoi diagram. A directed
@@ -55,19 +179,20 @@ class World(object):
             self.cells.append(cell)
         
         # Find neighbors; if cells share any vertices then they're neighbors
-        for source in self.cells:
-            for dest in self.cells:
-                source_region = set( self.vor.regions[source.region_idx] )
-                dest_region = set( self.vor.regions[dest.region_idx] )
+        # for source in self.cells:
+        #     for dest in self.cells:
+        #         if source.region_idx != dest.region_idx:
+        #             source_region = set( self.vor.regions[source.region_idx] )
+        #             dest_region = set( self.vor.regions[dest.region_idx] )
 
-                if -1 in source_region:
-                    source_region.remove(-1)
-                if -1 in dest_region:
-                    dest_region.remove(-1)
+        #             if -1 in source_region:
+        #                 source_region.remove(-1)
+        #             if -1 in dest_region:
+        #                 dest_region.remove(-1)
 
-                if not source_region.isdisjoint(dest_region):
-                    source.add_edge(dest)
-                    dest.add_edge(source)
+        #             if not source_region.isdisjoint(dest_region):
+        #                 source.add_edge(dest)
+        #                 dest.add_edge(source)
 
     '''
     A boundary cell is a cell that extends beyond the edge of the world.
@@ -91,8 +216,8 @@ class World(object):
     All cells at the edge of the map are water. We then traverse the graph from each of the boundary cells
     and mark cells along the path as water (% chance).
     '''
-    def _add_ocean(self):
-        for cell in self.cells:
+    def _add_ocean(self, region):
+        for cell in region.cells:
             if cell.is_boundary:
                 cell.type = Cell.Type.WATER
 
@@ -101,16 +226,16 @@ class World(object):
                 # but the number should probably be a function of the number of cells.
                 neighbor = cell
 
-                iterations = round( math.sqrt(math.sqrt(len(self.cells))) )
+                iterations = round( math.sqrt(math.sqrt(len(region.cells))) )
 
                 for _ in range(iterations):
-                    neighbor = random.choice(neighbor.neighbor_to)
+                    neighbor = random.choice(region.neighbors(neighbor.region_idx))
 
                     if (random.random() < 0.8):
                         neighbor.type = Cell.Type.WATER
 
-    def _terrainify(self):
-        for cell in self.cells:
+    def _terrainify(self, region):
+        for cell in region.cells:
             if not cell.type == Cell.Type.WATER:
                 cell.type = Cell.Type.LAND
 
@@ -194,7 +319,7 @@ class World(object):
         y = list( map(lambda p: p[1], region) )
         plt.fill(x, y, color)
 
-    def render(self, cell_labels=False, color_boundaries=False, cell_elevation=False):
+    def render(self, cell_labels=False, color_boundaries=False, cell_elevation=False, tectonics=False, show_graph=False):
         voronoi_plot_2d(self.vor, show_vertices=False, show_points=False, line_width=0)
         axes = plt.gca()
 
@@ -225,5 +350,27 @@ class World(object):
                 if cell.type == Cell.Type.LAND:
                     self.paint_cell(cell.region_idx, gradient[cell.elevation - 1].hex)
 
-        plt.title('Voronoi polygons')
+        if tectonics:
+            num_plates = len( set([c.plate_id for c in self.cells]) )
+
+            print('num_plates=%d' % (num_plates))
+            random_color = lambda: colour.rgb2hex( (random.random(), random.random(), random.random()) )
+            colors = [ random_color() for plate in range(num_plates) ]
+            
+            for cell in self.cells:
+                # self.paint_cell(cell.region_idx, colors[cell.plate_id])
+
+                plt.text(cell.location[0], cell.location[1], cell.plate_id, fontsize=8)
+
+        if show_graph:
+            for node in self.cells[0:100]:
+                for neighbor in node.neighbor_to:
+                    x = (node.location[0], neighbor.location[0])
+                    y = (node.location[1], neighbor.location[1])
+
+                    plt.plot(x, y, linestyle='dashed', color='red', linewidth=1)
+
+                plt.plot(node.location[0], node.location[1], 'bo')
+
+        plt.title('Rendered world')
         plt.show()
