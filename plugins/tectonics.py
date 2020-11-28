12e32297c2b7ef9ce64be9dd81b66ee8f2ea4fbc
anyweez <luke@lukesegars.com>
2020-11-28 18:48:03 -0500

    Implemented plate elevation (first part of #21).
    Also massively improved the performance of plate generation (~20-30s => 100ms) and
    improved plate quality! Prior to this change, plates extended across other plates
    which led to a patchy pattern that looked awful when you apply plate-based elevation
    rules.

--- plugins/tectonics.py
+++ plugins/tectonics.py
@@ -13,7 +13,7 @@ def generate(world, vd):
 
     # Configuration
     LandformConfig = {
-        'InitialPlateSplitProb': random.random() / 50.0, # [0, .02]
+        'InitialPlateSplitProb': 0.05,
         'InitialContinentMin': random.randint(1, 3),
         'InitialContinentMax': random.randint(3, 8),
     }
@@ -28,53 +28,57 @@ def generate(world, vd):
     )
 
     plate_centers = random.choices( list(world.cell_idxs()), k=num_plates )
-    plate_dist = [1,] * num_plates  # distance to go out from the center to find available cells
+    plate_dist = [1,] * num_plates      # distance to go out from the center to find available cells
+
+    unlabeled = set(world.cell_idxs())  # all cells start off as unlabeled
 
     for plate_idx, region_idx in enumerate(plate_centers):
         plates_arr[region_idx] = plate_idx
+        unlabeled.discard(region_idx)
 
-    # Function to check whether a cell still needs to have a plate assigned. Boundary cells
-    # are excluded.
-    still_available = lambda idx: plates_arr[idx] == Unassigned
-
-    remaining = sum( [1 for idx in world.cell_idxs() if still_available(idx)] )
     graph = world.graph
 
     # Add all cells to a plate
-    while remaining > 0:
+    while len(unlabeled) > 0:
         # For each plate, expand out from the center
         for center_idx in plate_centers:
             plate_id = plates_arr[center_idx]
 
-            avail = list( filter(still_available, graph.neighbors(center_idx, plate_dist[plate_id])) )
+            avail = list( filter(lambda idx: idx in unlabeled, graph.neighbors(center_idx, plate_dist[plate_id])) )
 
-            while len(avail) == 0:
-                plate_dist[plate_id] += 1
-                avail = list( filter(still_available, graph.neighbors(center_idx, plate_dist[plate_id])) )
+            # If there are no neighbors in the current radius, try expanding once. If there's nothing there
+            # if means we're surrounded and should skip from here on out.
+            if len(avail) == 0:
+                avail = list( filter(lambda idx: idx in unlabeled, graph.neighbors(center_idx, plate_dist[plate_id] + 1)) )
 
-            selected_idx = random.choice(avail)
+                # If there are neighbors to mark, continue. If not, move on to the next plate.
+                if len(avail) > 0:
+                    plate_dist[plate_id] += 1
+                else:
+                    continue
 
-            # The selected cell becomes part of the plate
-            plates_arr[selected_idx] = plates_arr[center_idx]
+            # The selected cells becomes part of the plate
+            for idx in avail:
+                plates_arr[idx] = plates_arr[center_idx]
+                unlabeled.discard(idx)
 
             # Count remaining cells to be marked; break the loop if we're done.
-            remaining = sum( [1 for idx in world.cell_idxs() if still_available(idx)] )
-
-            if remaining == 0:
+            if len(unlabeled) == 0:
                 break
 
         # There's a chance to add a new plate each iteration. New plates
         # can only exist in unmarked cells.
-        if random.random() < (LandformConfig['InitialPlateSplitProb'] / len(plate_centers)):
-            avail = list( filter(still_available, world.cell_idxs()) )
+        if random.random() < LandformConfig['InitialPlateSplitProb']:
+            # avail = list( filter(lambda idx: idx in unlabeled, world.cell_idxs()) )
 
-            if len(avail) > 0:
-                cell_idx = random.choice(avail)
+            if len(unlabeled) > 0:
+                cell_idx = random.choice(list(unlabeled))
 
                 # Reserve a new plate_id                
                 plates_arr[cell_idx] = len(plate_centers)
-                plate_centers.append(cell_idx)
-                plate_dist.append(1) # all plates start at dist=1
+                plate_centers.append(cell_idx)      # add to plate centers so it joins the core rotation
+                plate_dist.append(1)                # all plates start at dist=1
+                unlabeled.discard(cell_idx)         # cell_idx no longer available for labeling
 
     # Add cp_plate to the world
     world.add_cell_property('plate', plates_arr)
--- plugins/terrain.py
+++ plugins/terrain.py
@@ -17,8 +17,20 @@ def __gen_noise(x, y, config):
 
     return ( snoise2(scaled_x, scaled_y, octaves=octaves, persistence=persistence, lacunarity=lacunarity, base=base) + 1.0) / 2.0
 
+def randfloat(low, high):
+    diff = high - low
+    return (random.random() * diff) + low
 
-@genreq(cellprops=['latitude', 'longitude'])
+def cap(val, low, high):
+    if val < low:
+        return low
+    if val > high:
+        return high
+    
+    return val
+
+
+@genreq(cellprops=['latitude', 'longitude', 'plate'])
 def generate(world, vd):
     '''
     This plugin has two functions:
@@ -37,15 +49,28 @@ def generate(world, vd):
         'base': random.randint(0, 1000)
     }
 
-    WaterlineHeight = (random.random() / 4.0) + 0.3 # range = [0.3, 0.55)
+    WaterlineHeight = randfloat(0.2, 0.55)
 
     world.set_param('WaterlineHeight', WaterlineHeight)
 
-    # Calculate elevation using simplex noise
-    elevation_arr = world.new_cp_array(numpy.double, default_value=0)
+    # Calculate elevation. Contributions from plate tectonics and simplex noise.
+    noise_contrib = 0.70
+    plate_contrib = 0.30
 
-    for idx in world.cell_idxs():
-        elevation_arr[idx] = __gen_noise(world.cp_latitude[idx], world.cp_longitude[idx], NoiseConfig)
+    def init_elev(idx):
+        return __gen_noise(world.cp_latitude[idx], world.cp_longitude[idx], NoiseConfig)
+
+    elevation_list = [init_elev(idx) * noise_contrib for idx in world.cell_idxs()]
+
+    # Adjust height of cells based on the plate they're a part of
+    num_plates = max(world.cp_plate) + 1
+    plate_height = [randfloat(plate_contrib * -1, plate_contrib) for _ in range(num_plates)]
+
+    def shift_by_plate(idx, elevation):
+        return cap(plate_height[world.cp_plate[idx]] + elevation, 0.0, 1.0)
+
+    elevation_list = [ shift_by_plate(idx, elev) for idx, elev in enumerate(elevation_list) ]
+    elevation_arr = world.new_cp_array(numpy.double, elevation_list)
 
     def celltype(elevation):
         return Cell.Type.LAND if elevation > WaterlineHeight else Cell.Type.WATER
@@ -53,7 +78,7 @@ def generate(world, vd):
     # Determine which cells are land vs water
     celltype_arr = world.new_cp_array(
         None, 
-        [ celltype(elevation_arr[idx]) for idx in world.cell_idxs() ],
+        [ celltype(elevation_list[idx]) for idx in world.cell_idxs() ],
     )
 
     world.add_cell_property('elevation', elevation_arr)
